f87348da58424b750c0692d8fd758d4f6a779761
Eric Kappotis <ekappotis@foliage.com>
2017-05-25 06:22:38 -0400

    Fixed issue dealing with infinite recursion in some cases.

--- lib/declarative_authorization/in_controller.rb
+++ lib/declarative_authorization/in_controller.rb
@@ -613,7 +613,7 @@ module Authorization
       protected
       def filter_access_permissions # :nodoc:
         unless filter_access_permissions?
-          ancestors[1..-1].reverse.each do |mod|
+          (ancestors - [self] ).reverse.each do |mod|
             mod.filter_access_permissions if mod.respond_to?(:filter_access_permissions, true)
           end
         end
